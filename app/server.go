a1b0f6dbc260c4190363cc75c6d6b73d450c3bc5
Stupnikjs <n.boudier.ph@gmail.com>
2024-05-22 23:09:06 +0200

    ok

--- app/server.go
+++ app/server.go
@@ -1,47 +1,48 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
-	"io"
 	"net"
 	"os"
+	"strings"
 )
 
+func HandleRequest(conn net.Conn) {
+	// Read the request
+	buffer := make([]byte, 1024)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading: ", err.Error())
+		return
+	}
+	reader := bufio.NewReader(bytes.NewBuffer(buffer))
+	requestLine, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+	requestFields := strings.Fields(requestLine)
+	if requestFields[1] == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+}
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-
+	defer l.Close()
 	conn, err := l.Accept()
-
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	ReadResp(conn)
-	//conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-
-}
-
-func ReadResp(conn net.Conn) {
-	str := ""
-	for {
-		buf := make([]byte, 1)
-		_, err := conn.Read(buf)
-
-		if err == io.EOF {
-			break
-		}
-		str += string(buf)
-
-	}
-	print("str: ", str)
-
+	defer conn.Close()
+	HandleRequest(conn)
 }
